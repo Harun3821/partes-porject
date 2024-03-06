b3346022f841abb4aceff4ccefca664d7c42689b
Harun3821 <harun4382183@gmail.com>
2024-03-06 20:36:13 +0600

    first commit card steringfy object

--- part.js
+++ part.js
@@ -1,4 +1,50 @@
-// // data access
-// const data = [{id: 1, name: 'abul', address: 'hazipara'}]
+const adProdctes = () =>{
+    const prodectFilled = document.getElementById('prodctes-name');
+    const prodctesQuantity = document.getElementById('prodctes-quantity');
+    const prodctes = prodectFilled.value;
+    const quantity = prodctesQuantity.value;
+    prodectFilled.value = '';
+    prodctesQuantity.value = '';
+    displyProdect(prodctes, quantity);
+    saveprodecttoLocalstorage(prodctes, quantity)
+   
+}
 
-// console.log(data[0].address);
+
+
+const displyProdect = (prodctes, quantity) =>{
+    const listProdect = document.getElementById('prodect-continer');
+    const li = document.createElement('li')
+    li.innerText = `${prodctes} ${quantity}`
+    listProdect.appendChild(li);
+}
+
+const getstrodeCard = () =>{
+    let card = {}
+    const strotCard = localStorage.getItem('card');
+    if(strotCard){
+       card = JSON.parse(strotCard)
+    }
+    return card;
+}
+
+const saveprodecttoLocalstorage = (prodctes, quantity) =>{
+    const card = getstrodeCard();
+    card [prodctes] = quantity;
+    const cardSteringfy = JSON.stringify(card);
+    console.log(cardSteringfy)
+    localStorage.setItem('card', cardSteringfy);
+ 
+}
+
+const displyFormlocalStroge = () =>{
+    const saveCard = getstrodeCard()
+    console.log(saveCard);
+    for(const prodctes in saveCard){
+        const quantity = saveCard[prodctes];
+        console.log(prodctes, quantity);
+        displyProdect(prodctes, quantity)
+    }
+}
+
+displyFormlocalStroge();
